7a3801e77ca3494ac9911aea5e1be4694c0da906
Rafael Freitas <vidarafael@hotmail.com>
2021-08-23 21:06:33 -0300

    adicionando paginaçao

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -3,11 +3,15 @@ import Link from "next/link";
 
 import { SubmitHandler, useForm } from 'react-hook-form'
 import * as yup from 'yup'
-import { yupResolver } from '@hookform/resolvers/yup'
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useMutation } from 'react-query';
 
 import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
+import { useRouter } from "next/dist/client/router";
 
 type CreateUserFormData = {
   name: string;
@@ -26,6 +30,23 @@ const createUserFormSchema = yup.object().shape({
 })
 
 export default function CreateUser() {
+  const router = useRouter()
+
+  const createUser = useMutation(async (user: CreateUserFormData) => {
+    const response = await api.post('users', {
+      user: {
+        ...user,
+        created_at: new Date(),
+      }
+    })
+
+    return response.data.user;
+  }, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('users')
+    }
+  })
+
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(createUserFormSchema)
   })
@@ -33,9 +54,9 @@ export default function CreateUser() {
   const { errors } = formState
 
   const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
-    await new Promise(resolve => setTimeout(resolve, 2000))
+    await createUser.mutateAsync(values)
 
-    console.log(values)
+    router.push('/users')
   }
 
   return (
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model, Response } from 'miragejs';
+import { createServer, Factory, Model, Response, ActiveModelSerializer } from 'miragejs';
 import faker from 'faker'
 
 type User = {
@@ -9,6 +9,10 @@ type User = {
 
 export function makeServer() {
   const server = createServer({
+    serializers: {
+      application: ActiveModelSerializer,
+    },
+
     models: {
       user: Model.extend<Partial<User>>({})
     },
@@ -28,7 +32,7 @@ export function makeServer() {
     },
 
     seeds(server) {
-      server.createList('user', 10)
+      server.createList('user', 200)
     },
 
     routes() {
@@ -53,6 +57,7 @@ export function makeServer() {
         )
       });
 
+      this.get('/users/:id')
       this.post('/users');
 
       this.namespace = '';
